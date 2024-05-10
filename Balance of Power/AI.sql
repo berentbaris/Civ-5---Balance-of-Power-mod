229eeb8ba4974089538effe75ccbef4653c63eec
berentbaris <94933586+berentbaris@users.noreply.github.com>
2024-05-10 11:51:15 +0200

    Add files via upload

--- Balance of Power/AI.sql
+++ Balance of Power/AI.sql
@@ -1,12 +1,11 @@
-UPDATE [leaders] SET [MinorCivCompetitiveness] = [MinorCivCompetitiveness] + 2;
 UPDATE [leaders] SET [VictoryCompetitiveness] = [VictoryCompetitiveness] + 4;
 UPDATE [leaders] SET [Boldness] = [Boldness] + 2;
 
-UPDATE [Leader_MajorCivApproachBiases] SET [Bias] = [Bias] + 2 WHERE MajorCivApproachType = 'MAJOR_CIV_APPROACH_WAR';
+UPDATE [Leader_MajorCivApproachBiases] SET [Bias] = [Bias] + 1 WHERE MajorCivApproachType = 'MAJOR_CIV_APPROACH_WAR';
 UPDATE [Leader_MajorCivApproachBiases] SET [Bias] = [Bias] + 1 WHERE MajorCivApproachType = 'MAJOR_CIV_APPROACH_HOSTILE';
-UPDATE [Leader_MajorCivApproachBiases] SET [Bias] = [Bias] - 4 WHERE MajorCivApproachType = 'MAJOR_CIV_APPROACH_AFRAID';
+UPDATE [Leader_MajorCivApproachBiases] SET [Bias] = [Bias] - 8 WHERE MajorCivApproachType = 'MAJOR_CIV_APPROACH_AFRAID';
 
-UPDATE [Leader_Flavors] SET [Flavor] = [Flavor] + 3 WHERE [FlavorType] = 'FLAVOR_OFFENSE';
+UPDATE [Leader_Flavors] SET [Flavor] = [Flavor] + 2 WHERE [FlavorType] = 'FLAVOR_OFFENSE';
 UPDATE [Leader_Flavors] SET [Flavor] = [Flavor] + 2 WHERE [FlavorType] = 'FLAVOR_DEFENSE';
 UPDATE [Leader_Flavors] SET [Flavor] = [Flavor] + 2 WHERE [FlavorType] = 'FLAVOR_RANGED';
 UPDATE [Leader_Flavors] SET [Flavor] = [Flavor] + 2 WHERE [FlavorType] = 'FLAVOR_MOBILE';
